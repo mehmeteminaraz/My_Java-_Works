0d263ff3aba0613557b8f06bd60cd6e2a14e0e4f
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-06-25 19:16:29 +0300

    34

---
+++ src/C34_TStringManipulation.java
@@ -0,0 +1,18 @@
+public class C34_TStringManipulation {
+
+    public static void main(String[] args) {
+        String str1= "Java";
+        String str2=new String("java");
+        // 10. satir
+        if(str2.equals(str1.toLowerCase()))
+
+
+        {
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+
+        }
+
+    }
+}
