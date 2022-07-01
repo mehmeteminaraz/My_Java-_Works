b12d18451d89417a0e2661f3ac4399a3e8db1706
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-07-01 22:27:44 +0300

    39

---
+++ src/C39_StringManipulations02.java
@@ -0,0 +1,20 @@
+public class C39_StringManipulations02 {
+    public static void main(String[] args) {
+
+        String s = "purr";
+        s.toUpperCase();
+        s.trim();
+        s.substring(1, 3);
+        s += " two";
+        System.out.println(s.length());
+
+        /*
+        A. 2
+        B. 4
+        C. 8
+        D. 10
+        E. Kod exception olusturur.
+        F. Kod calismaz.
+        */
+    }
+}
