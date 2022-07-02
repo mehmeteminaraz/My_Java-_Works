5fdcbf1b881a9e45233e3bc4bb41e9853162bfa1
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-07-02 23:20:00 +0300

    40

---
+++ src/C40_methodCreation.java
@@ -0,0 +1,17 @@
+public class C40_methodCreation {
+    public static void main(String[] args) {
+
+    }
+    /*
+    public void methodA() { return;}
+    public void methodB() { return null;}
+    public void methodD() {}
+
+
+    public int methodE() { return 9;}
+    public int methodF() { return 9.0;}
+    public int methodG() { return;}
+    public int methodH() { return null;}
+
+     */
+}
