a3d9190fbf42912adadbfaf66158f5d8ee0fa227
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-07-14 11:17:19 +0300

    50

---
+++ src/C50_Arrays01Cozum.java
@@ -0,0 +1,19 @@
+public class C50_Arrays01Cozum {
+    public static void main(String[] args) {
+
+
+        int[][] scores = new int[5][];
+        // [null, null, null, null, null]
+
+        Object[][][] cubbies = new Object[3][0][5];
+        // [[], [], []]
+
+        Object[][][] cubbies2 = new Object[3][1][5];
+        // [[[null, null, null, null, null]], [[null, null, null, null, null]], [[null, null, null, null, null]]]
+
+
+
+
+
+    }
+}
