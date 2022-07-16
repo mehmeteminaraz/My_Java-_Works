d3052a04602afff0818e6ce5ebd8703add5c00fb
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-07-16 09:29:19 +0300

    53

---
+++ src/C53_Arrays03.java
@@ -0,0 +1,18 @@
+public class C53_Arrays03 {
+    public static void main(String[] args) {
+        char[]c = new char[2];
+        // INSERT CODE HERE
+        int length = c.length;
+        System.out.println(length);
+
+        /*
+        A. int length = c.capacity;
+        B. int length = c.capacity();
+        C. int length = c.length;
+        D. int length = c.length();
+        E. int length = c.size;
+        F. int length = c.size();
+        G. None of the above.
+         */
+    }
+}
