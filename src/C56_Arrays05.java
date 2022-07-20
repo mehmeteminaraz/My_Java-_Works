1c13a46a0bab2d3be2ca6f8389901ec5cf1a5dba
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-07-20 12:32:30 +0300

    56

---
+++ src/C56_Arrays05.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class C56_Arrays05 {
+    public static void main(String[] args) {
+        int[] random = { 16, -41, 112, 10, -110 };
+
+        int x = 140;
+        int y = Arrays.binarySearch(random, x);
+        System.out.println(y);
+    }
+}
+
+    /*
+      A.  2
+      B.  3
+      C.  6
+      D.  Sonuc belirlenemez.
+      E.  Bir exception olusur
+      F.  Kod calismaz.
+     */
