c27df466cd91f42e80f637c718e32f5e329ff3ac
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-07-21 16:23:00 +0300

    57

---
+++ src/C57_Arrays06.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class C57_Arrays06 {
+    public static void main(String[] args) {
+        String[] fruits = {
+                "banana",
+                "apple",
+                "pears",
+                "grapes"
+        };
+        Arrays.sort(fruits);
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.print(fruits[i] + " ");
+
+        }
+
+    }
+}
+
+    /*
+        A) apple banana grapes pears
+        B) pears grapes banana apple
+        C) banana apple pears grapes
+        D) Kod calismaz
+     */
