9bb0aa3ce9e884bf66eb075a643a1db0d0611306
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-10-18 22:14:38 +0300

    88

---
+++ src/methods/ReturnMethod.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class ReturnMethod {
+    public static void main(String[] args) {
+        /*
+         Input olarak verilen isim ve soyismi
+         ilk harfleri buyuk, diger harfler kucuk olarak degistirip
+         programin sonraki bolumlerinde kullanabilmek uzere
+         isimSoyisim variable'ina atayacak bir method olusturun
+         input - Isim : ALI, Soyisim: CAN
+         Output- isimSoyisim : Ali Can seklinde kayit
+         */
+
+        String isim="semra";
+        String soyisim= "koca";
+
+
+    }
+
+}
