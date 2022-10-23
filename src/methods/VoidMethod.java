4aa6db33761c0bd8aa8f76cacb3b4c99c04f9de7
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-10-23 22:41:12 +0300

    55

---
+++ src/methods/VoidMethod.java
@@ -0,0 +1,19 @@
+package methods;
+public class VoidMethod {
+    public static void main(String[] args) {
+        /*
+           Input olarak verilen satis fiyati, musteri karti var mi ?
+           ve uyeligi kac yillik ? bilgilerini degerlendirip
+           - uye degilse : %5 indirim
+           - uyeligi var ama 5 yildan az ise %10 indirim
+           - uyeligi var ve 5 yildan cok ise %15 indim uygulayip
+           asil fiyati, indirim miktarini ve indirimli fiyati yazdiran bir method olusturun
+         */
+
+        boolean uyeMi=false;
+        int uyelikYili=13;
+        double satisFiyati=250;
+
+    }
+
+}
